7783b46f7aa16c9a70042c6374537083f86100e1
Perezented <mperez6473@gmail.com>
2020-03-24 15:30:40 -0500

    Pulled information from swapi. Had to step away for a bit.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 function App() {
+  axios.get('https://swapi.co/api/people/')
+  .then(res=>{
+      console.log(res.data.results)
+      const MyTeam = res.data.results
+    }
+
+  )
+  .catch(error=> console.log('errrrrroroorororoorororrrrr', error))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>List of people</h2>
+      {MyTeam.map(res=> {console.log(res)
+        return <h5>{res.name}</h5>
+      })}
+
     </div>
   );
 }
