3d00917c4298aea12e8078634bf0cbdb554cb250
Perezented <mperez6473@gmail.com>
2020-03-24 20:46:12 -0500

    work on this computer. Not much but going to get my laptop back up and running and work on this..

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import axios from 'axios';
 
 function App() {
-  axios.get('https://swapi.co/api/people/')
-  .then(res=>{
-      console.log(res.data.results)
-      const MyTeam = res.data.results
-    }
-
-  )
-  .catch(error=> console.log('errrrrroroorororoorororrrrr', error))
-
   return (
     <div className="App">
-      <h2>List of people</h2>
-      {MyTeam.map(res=> {console.log(res)
-        return <h5>{res.name}</h5>
-      })}
-
+      <h1>List of People</h1>
     </div>
   );
 }
